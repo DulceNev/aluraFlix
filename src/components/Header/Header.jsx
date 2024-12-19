17681e1daf5cd04ad63becc3958c1e9b62046e2b
Dulce Nevarez <dulcenev04@gmail.com>
2024-12-19 16:55:55 -0500

    se creo el css del header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+import { FaHome } from "react-icons/fa";
+import { RiAddCircleLine } from "react-icons/ri";
+
+const Header = () => {
+    return <header className='header'>
+        <img className='logo' src="../src/assets/Logo.png" alt="AluraFlix" />
+
+        <div className='button-section-pc'>
+            <button className='button'>Home</button>
+            <button className='button'>Nuevo video</button>
+        </div>
+
+        <div className='button-section-movil'>
+            <FaHome color="white" size="2em" />
+            <RiAddCircleLine color="white" size="2em" />
+        </div>
+    </header>
+}
+
+export default Header
