903d5e2f4247456a7035e519153d4e470976a741
Dulce Nevarez <dulcenev04@gmail.com>
2025-01-02 17:29:15 -0500

    fix: update logo import paths in Footer and Header components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import './Footer.css';
-
+import Logo from "../../assets/Logo.png";
 const Footer = () => {
     return (
         <footer className='footer py-3'>
-            <img className='logo-footer ' src="../src/assets/Logo.png" alt="" />
+            <img className='logo-footer ' src={Logo} alt="" />
         </footer>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,11 @@ import './Header.css';
 import { FaHome } from "react-icons/fa";
 import { RiAddCircleLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
-
+import Logo from "../../assets/Logo.png";
 const Header = () => {
 
     return <header className='header'>
-        <img className='logo' src="../src/assets/Logo.png" alt="AluraFlix" />
+        <img className='logo' src={Logo} alt="AluraFlix" />
 
         <div className='button-section-pc'>
             <Link to="/" className='buttonHeader'>Home</Link>
